a724df62a0526e2b30b87a6652e0fbb91546e97d
tburd77 <46765026+tburd77@users.noreply.github.com>
2023-11-10 14:38:11 -0800

    Update DigitalOutputs.ino

--- DigitalOutputs.ino
+++ DigitalOutputs.ino
@@ -1,1 +1,21 @@
+#include "TempHumidityClock_ESP32.h"
+
+    void setupDigitalOutputs() {
+        pinMode(heaterOn_Pin, OUTPUT);//2
+        pinMode(fanOn_Pin, OUTPUT);//4
+    }
+    
+    void OnOff_TempHumid() {
+        if (thermoStatus == true) {
+            digitalWrite(heaterOn_Pin, HIGH);
+        }else {
+            digitalWrite(heaterOn_Pin, LOW);
+        }
+        
+        if (fanStatus == true) {
+            digitalWrite(fanOn_Pin, HIGH);
+        }else {
+            digitalWrite(fanOn_Pin, LOW);
+        }
+    }
 
